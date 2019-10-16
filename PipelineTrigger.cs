6fea4e96b6dbdec9855900f08d2055ab2b0e0274
Brian <brianhungerman@gmail.com>
2019-10-16 01:49:09 -0700

    Testing DB Connection with GetPlayers endpoint

--- PipelineTrigger.cs
+++ PipelineTrigger.cs
@@ -7,27 +7,86 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 
-namespace BitnaughtsFunctionApp
+namespace ModestoMoves
 {
-    public static class PipelineTrigger
+    public static class FunctionApp
     {
-        [FunctionName("PipelineTrigger")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log
-        ) {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+        [FunctionName("GetPlayers")]
+        public static async Task<JObject> GetPlayers(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "GetPlayers")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Running Racing Route");
+            string output = "";
 
-            string name = req.Query["name"];
+            string raceID = req.Query["raceID"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            List<decimal> latList = new List<decimal>();
+            List<decimal> longList = new List<decimal>();
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            SqlConnection connectionString = new SqlConnection(System.Environment.GetEnvironmentVariable("Connection String"));
+            SqlCommand querystring = new SqlCommand("SELECT alias FROM Players", connectionString);
+
+
+            try
+            {
+                using (connectionString)
+                {
+                    output = "Connection Established");
+                    connectionString.Open();
+
+                    SqlDataReader reader = querystring.ExecuteReader();
+
+                    Object[] values = new Object[reader.FieldCount];
+                    int fieldCount = reader.GetValues(values);
+
+                    output += "reader.GetValues retrieved " + fieldCount + "columns.";
+                    for (int i = 0; i < fieldCount; i++)
+                        output += values[i];
+                    
+                    reader.Close();
+                    connectionString.Close();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+            List<Decimal[]> cords = new List<Decimal[]>();
+            for(int i = 0; i < latList.Count; i++)
+            {
+                decimal[] arrayOfInt = {latList[i], longList[i]};
+                cords.Add(arrayOfInt);
+            }
+
+            var jobject = new
+            {
+                id = output,
+                type = "line",
+                source = new
+                {
+                    type = "geojson",
+                    data = new
+                    {
+                        type = "Feature",
+                        properties = new { },
+                        geometry = new
+                        {
+                            type = "LineString",
+                            coordinates = cords
+                        }
+
+                    }
+                }
+            };
+
+            return JObject.FromObject(jobject);
         }
     }
 }
