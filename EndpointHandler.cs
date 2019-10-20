a9b8ad52cdad17324a6f92a9bb3b75d59caea6aa
Brian <brianhungerman@gmail.com>
2019-10-20 03:35:04 -0700

    Ensuring IDisposables are handled, including dependencies, linting

--- EndpointHandler.cs
+++ EndpointHandler.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
@@ -10,7 +12,7 @@ using Nancy.Json;
 
 namespace BitNaughts {
     public static class EndpointHandler {
-        
+
         /* Result-formatting constants */
         public const string DELIMITER = ",";
         public const string NEW_LINE = "\n";
@@ -19,11 +21,11 @@ namespace BitNaughts {
         /* * * * * * * * * * */
 
         [FunctionName ("Create")] /* API Endpoint: /api/create?table=players */
-        public static async Task<string> Create ([HttpTrigger (AuthorizationLevel.Anonymous, "post", Route = "create")] HttpRequest req, ILogger log) {
+        public static async Task<string> Create ([HttpTrigger (AuthorizationLevel.Anonymous, "post", Route = "create")] HttpRequest req) {
 
             /* Reads data into table and returns transaction receipt */
             dynamic req_body = await GetBody (req);
-            return QueryHandler.ExecuteNonQuery (
+            return ExecuteNonQuery (
                 String.Format (
                     "INSERT INTO {0} VALUES ({1})", /* SQL Query to be executed */
                     req.Query["table"],
@@ -36,24 +38,29 @@ namespace BitNaughts {
         }
 
         [FunctionName ("Read")] /* API Endpoint: /api/read?table=players&fields=* */
-        public static async Task<string> Read ([HttpTrigger (AuthorizationLevel.Anonymous, "get", Route = "read")] HttpRequest req, ILogger log) {
+        public static async Task<string> Read ([HttpTrigger (AuthorizationLevel.Anonymous, "get", Route = "read")] HttpRequest req) {
 
             /* Returns formatted result of selection query */
-            return QueryHandler.ExecuteQuery (
+            dynamic req_body = await GetBody (req);
+            return ExecuteQuery (
                 String.Format (
-                    "SELECT {1} FROM {0}", /* SQL Query to be executed */
+                    "SELECT {1} FROM {0} WHERE {2}", /* SQL Query to be executed */
                     req.Query["table"],
-                    req.Query["fields"]
+                    String.Join (
+                        DELIMITER,
+                        req_body.values.ToObject<string[]> ()
+                    ),
+                    req_body.condition
                 )
             );
         }
 
         [FunctionName ("Update")] /* API Endpoint: /api/update?table=players */
-        public static async Task<string> Update ([HttpTrigger (AuthorizationLevel.Anonymous, "put", Route = "update")] HttpRequest req, ILogger log) {
+        public static async Task<string> Update ([HttpTrigger (AuthorizationLevel.Anonymous, "put", Route = "update")] HttpRequest req) {
 
             /* Overrides data in table and returns transaction receipt */
             dynamic req_body = await GetBody (req);
-            return QueryHandler.ExecuteNonQuery (
+            return ExecuteNonQuery (
                 String.Format (
                     "UPDATE {0} SET {1} WHERE {2}", /* SQL Query to be executed */
                     req.Query["table"],
@@ -67,11 +74,11 @@ namespace BitNaughts {
         }
 
         [FunctionName ("Delete")] /* API Endpoint: /api/delete?table=players */
-        public static async Task<string> Delete ([HttpTrigger (AuthorizationLevel.Anonymous, "delete", Route = "delete")] HttpRequest req, ILogger log) {
+        public static async Task<string> Delete ([HttpTrigger (AuthorizationLevel.Anonymous, "delete", Route = "delete")] HttpRequest req) {
 
             /* Removes data in table and returns transaction receipt */
             dynamic req_body = await GetBody (req);
-            return QueryHandler.ExecuteNonQuery (
+            return ExecuteNonQuery (
                 String.Format (
                     "DELETE FROM {0} WHERE {1}", /* SQL Query to be executed */
                     req.Query["table"],
@@ -85,55 +92,59 @@ namespace BitNaughts {
 
         /* Assembles HTTP Body byte-stream into JSON */
         public static async Task<dynamic> GetBody (HttpRequest req) {
-            string requestBody = await new StreamReader (req.Body).ReadToEndAsync ();
-            return Newtonsoft.Json.JsonConvert.DeserializeObject (requestBody);
+            using (var reader = new StreamReader (req.Body)) {
+                string body_stream = await reader.ReadToEndAsync ();
+                return Newtonsoft.Json.JsonConvert.DeserializeObject (body_stream);
+            }
         }
 
         /* Manages database connection, runs queries, and returns results */
         public static string ExecuteQuery (string query) {
             try {
                 /* Defines connection parameters and query logic */
-                SqlConnection connection = new SqlConnection (System.Environment.GetEnvironmentVariable ("Connection String"));
-                SqlCommand command = new SqlCommand (query, connection);
+                using (SqlConnection connection = new SqlConnection (System.Environment.GetEnvironmentVariable ("Connection String"))) {
+                    using (SqlCommand command = new SqlCommand (query, connection)) {
 
-                /* Connects to database and executes query */
-                connection.Open ();
-                SqlDataReader reader = command.ExecuteReader ();
+                        /* Connects to database and executes query */
+                        connection.Open ();
+                        using (SqlDataReader reader = command.ExecuteReader ()) {
 
-                /* Holds row results as they are read */
-                List<string> results = new List<string> ();
-                while (reader.Read ()) {
+                            /* Holds row results as they are read */
+                            List<string> results = new List<string> ();
+                            while (reader.Read ()) {
 
-                    /* Dumps values into Object array */
-                    Object[] fields = new Object[reader.FieldCount];
-                    reader.GetValues (fields);
+                                /* Dumps values into Object array */
+                                Object[] fields = new Object[reader.FieldCount];
+                                reader.GetValues (fields);
 
-                    /* Adds row result as delimiter-seperated values */
-                    results.Add (
-                        String.Join (
-                            DELIMITER,
-                            fields.Where (x => x != null)
-                            .Select (x => x.ToString ())
-                            .ToArray ()
-                        )
-                    );
+                                /* Adds row result as delimiter-seperated values */
+                                results.Add (
+                                    String.Join (
+                                        DELIMITER,
+                                        fields.Where (x => x != null)
+                                              .Select (x => x.ToString ())
+                                              .ToArray ()
+                                    )
+                                );
+                            }
+
+                            /* Closes the database connection */
+                            reader.Close ();
+                            connection.Close ();
+
+                            /* Returns formatted results from query */
+                            return String.Join (
+                                NEW_LINE,
+                                results.ToArray ()
+                            );
+                        }
+                    }
                 }
-
-                /* Closes the database connection */
-                reader.Close ();
-                connection.Close ();
-
-                /* Returns formatted results from query */
-                return String.Join (
-                    NEW_LINE,
-                    results.ToArray ()
-                );
-
             } catch (Exception ex) {
-                return String.Format(
+                return String.Format (
                     "Error({0}): {1}",
                     query,
-                    ex.ToString()
+                    ex.ToString ()
                 );
             }
         }
@@ -142,25 +153,26 @@ namespace BitNaughts {
         public static string ExecuteNonQuery (string query) {
             try {
                 /* Defines connection parameters and query logic */
-                SqlConnection connection = new SqlConnection (System.Environment.GetEnvironmentVariable ("Connection String"));
-                SqlCommand command = new SqlCommand (query, connection);
+                using (SqlConnection connection = new SqlConnection (System.Environment.GetEnvironmentVariable ("Connection String"))) {
+                    using (SqlCommand command = new SqlCommand (query, connection)) {
 
-                /* Connects to database and executes non-query */
-                connection.Open ();
-                int rows_modified = command.ExecuteNonQuery ();
+                        /* Connects to database and executes non-query */
+                        connection.Open ();
+                        int rows_modified = command.ExecuteNonQuery ();
 
-                /* Returns number of rows modified */
-                return String.Format (
-                    "Query({0}): {1} Row(s) Modified",
-                    query,
-                    rows_modified
-                );
-
+                        /* Returns number of rows modified */
+                        return String.Format (
+                            "Query({0}): {1} Row(s) Modified",
+                            query,
+                            rows_modified
+                        );
+                    }
+                }
             } catch (Exception ex) {
-                return String.Format(
+                return String.Format (
                     "Error({0}): {1}",
                     query,
-                    ex.ToString()
+                    ex.ToString ()
                 );
             };
         }
