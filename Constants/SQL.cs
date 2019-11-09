59887a800e5afa20c833602d47bb545e1c372493
Brian <brianhungerman@gmail.com>
2019-11-08 19:01:27 -0800

    Debugging: inserting values one at a time

--- Constants/SQL.cs
+++ Constants/SQL.cs
@@ -24,5 +24,5 @@ public static class SQL {
 
 
     /* MS SQL has been shown to perform best when inserting groups of 25 values at a time. See https://www.red-gate.com/simple-talk/sql/performance/comparing-multiple-rows-insert-vs-single-row-insert-with-three-data-load-methods/ */
-    public const int INSERT_BATCH_SIZE = 25;
+    public const int INSERT_BATCH_SIZE = 1;//25;
 }
