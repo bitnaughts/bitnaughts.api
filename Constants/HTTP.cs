9e9353860efa4f640b614580b828bb4aae91135d
Brian <brianhungerman@gmail.com>
2019-11-10 04:14:13 -0800

    Adding Mine endpoint parameters

--- Constants/HTTP.cs
+++ Constants/HTTP.cs
@@ -1,15 +1,25 @@
 public static class HTTP {
-    public const string GET = "get", POST = "post", DELETE = "delete", PUT = "put";
+    public const string GET = "get",
+        POST = "post",
+        DELETE = "delete",
+        PUT = "put";
+        
     public static class Endpoints {
         public const string GET = "get",
             SET = "set",
             UPDATE = "update",
-            RESET = "reset"; //DELETE = "delete", PUT = "put";
+            RESET = "reset",
+            MINE = "mine";
 
         public static class Parameters {
             public const string FLAG = "flag",
                 TYPE = "type",
-                ID = "id";
+                ID = "id",
+                ASTEROID = "asteroid",
+                SHIP = "ship",
+                AMOUNT = "amount",
+                DATE = "date";
+
             public static class Values {
                 public const string RESET = "reset",
                     TYPE = "type",
