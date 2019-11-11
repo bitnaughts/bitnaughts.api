af74c17cb90ed0ecba7c8ca061b4643a9a93bc70
Brian <brianhungerman@gmail.com>
2019-11-10 21:59:19 -0800

    Adding more example endpoints

--- api-all-code.sql
+++ api-all-code.sql
@@ -1,17 +1,30 @@
---api/mine?asteroid=11&ship=0&amount=20
+-- api/visit?planet=12&ship=0
+
+INSERT INTO dbo.Visits
+VALUES
+    (0, 12, '2019-11-11 05:31:57.491');
+
+-- api/mine?asteroid=11&ship=0&amount=20
 
 -- Mining alway part of an asteroid
 UPDATE dbo.Asteroids SET a_size = 6.59 WHERE a_asteroid_id = 11;
 UPDATE dbo.Ships SET sp_data = 1.00 WHERE sp_ship_id = 0;
+INSERT INTO dbo.Mines
+VALUES
+    (0, 11, 1.00, '2019-11-11 05:28:20.408');
 
 -- Mining an asteroid completely
-DELETE FROM dbo.Asteroids WHERE a_asteroid_id = 12
-UPDATE dbo.Ships SET sp_data = 0.20 WHERE sp_ship_id = 0
-
+DELETE FROM dbo.Asteroids WHERE a_asteroid_id = 12;
+UPDATE dbo.Ships SET sp_data = 0.20 WHERE sp_ship_id = 0;
+INSERT INTO dbo.Mines
+VALUES
+    (0, 12, .20, '2019-11-11 05:28:24.243');
 
 -- api/set?flag=add&table=dbo.ships
 
-INSERT INTO dbo.Ships VALUES (0,1,1,'ShipX','',4.24,2.42);
+INSERT INTO dbo.Ships
+VALUES
+    (0, 1, 1, 'ShipX', '', 4.24, 2.42);
 
 -- api/set?flag=reset
 
---
+++ api-visit-excuted-code.sql
@@ -0,0 +1,5 @@
+-- api/visit?planet=12&ship=0
+
+INSERT INTO dbo.Visits
+VALUES
+    (0, 12, '2019-11-11 05:31:57.491');
